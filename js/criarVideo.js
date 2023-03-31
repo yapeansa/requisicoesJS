10556c34e230e7ed17d893f3411533a56a04d2cd
Yago <yago.pereira@estudante.ufjf.br>
2023-03-30 21:42:15 -0300

    Javascript. Requisições.

---
+++ js/criarVideo.js
@@ -0,0 +1,15 @@
+import { conectaAPI } from './conectaAPI.js';
+
+const formulario = document.querySelector('[data-formulario]');
+
+async function criarVideo (evento) {
+    evento.preventDefault();
+    const imagem = document.querySelector('[data-imagem]').value;
+    const url = document.querySelector('[data-url]').value;
+    const titulo = document.querySelector('[data-titulo]').value;
+    const descricao = Math.floor(Math.random() * 10).toString();
+    await conectaAPI.criaVideo(titulo, descricao, url, imagem);
+    window.location.href = '../pages/envio-concluido.html';
+}
+
+formulario.addEventListener('submit', evento => criarVideo(evento));
